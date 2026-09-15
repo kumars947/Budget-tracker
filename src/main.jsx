ab05de89743d0402912cd300e1cee5bbba2cfefe
kumars947 <kumars090407@gmail.com>
2026-09-15 08:27:49 +0530

    Refactor main.jsx to use React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-import App from "./App";
-import "./index.css";
-
-createRoot(
-  document.getElementById("root")
-).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>
+)
